3e1542e9ef1b4c8e1bb08bd4900b5911bd15bcca
Amber Pittman <amber@amberpittman.com>
2019-04-22 12:58:19 -0400

    Completed C2 on Array Methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -69,8 +69,13 @@ runners.forEach(function(fullName) {
 // ==== Challenge 2: Use .map() ====
 /* The event director needs to have all the runner's first names converted to uppercase because the director 
 BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result*/
-//let allCaps = [];
-//console.log(allCaps); 
+let allCaps = [];
+
+runners.map(function(item) {
+    allCaps.push(item.first_name.toUpperCase());
+});
+
+console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 /* The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with 
