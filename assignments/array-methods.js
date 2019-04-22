378698c1c6f9a0feaa8fd331601a6f7418889aa3
Amber Pittman <amber@amberpittman.com>
2019-04-22 13:18:25 -0400

    Completed C3 on Array Methods

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -81,8 +81,14 @@ console.log(allCaps);
 /* The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with 
 large sized shirts so they can choose a different size. Return an array named largeShirts that contains 
 information about the runners that have a shirt size of L and log the result */
-//let largeShirts = [];
-//console.log(largeShirts);
+let largeShirts = [];
+
+runners.filter(function(item) {
+    if (item.shirt_size === "L") {
+        largeShirts.push({first_name: item.first_name, last_name: item.last_name, shirt_size: item.shirt_size});
+      };
+});
+console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 /* The donations need to be tallied up and reported for tax purposes. Add up all the donations into a 
